5bdeb10745cc3eec139f13bd459a868c55c9604b
Subhash Mahimaluri <subbumahimaluri@gmail.com>
2022-04-20 17:09:02 +0530

    CircleCI config changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      Learn React
+      React CICD POC for Yexaa
     </div>
   );
 }
